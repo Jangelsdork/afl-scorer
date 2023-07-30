8d82c0fe8ceeceb860ab403f67a9d7cfdb3bd4b7
jangelsdork <j.hespe.mangelsdorf@gmail.com>
2023-07-30 18:10:10 +0200

    first commit

---
+++ components/NewGame.tsx
@@ -0,0 +1,7 @@
+export default function NewGame(){
+    return (
+        <div className="bg-yellow-300 w-50vw h-50vw border-yellow-400 p-4 m-4 rounded">
+            <h2>Add New Game</h2>
+        </div>
+    )
+}
